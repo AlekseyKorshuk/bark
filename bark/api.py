89ea766404df27e60b3fa19e3650db47425a3250
Aleksey Korshuk <ale-kor02@mail.ru>
2023-05-20 23:22:29 -0700

    add streaming

--- bark/api.py
+++ bark/api.py
@@ -164,4 +164,11 @@ def generate_audio_stream(
         else:
             fine_tokens = np.concatenate([fine_tokens, batch_fine_tokens], axis=1)
     audio_arr = codec_decode(fine_tokens)
-    return audio_arr
+    write_wav(f"bark_generation_merged.wav", SAMPLE_RATE, audio_arr)
+    final_fine_tokens = generate_fine(
+        coarse_tokens,
+        history_prompt=history_prompt,
+        temp=0.5,
+    )
+    final_audio_arr = codec_decode(final_fine_tokens)
+    return final_audio_arr
