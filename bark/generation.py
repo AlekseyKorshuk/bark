3e155a64347fdea07271e084b3b79bb63f162ec9
Aleksey Korshuk <ale-kor02@mail.ru>
2023-05-21 22:33:19 -0700

    add streaming

--- bark/generation.py
+++ bark/generation.py
@@ -14,15 +14,15 @@ import tqdm
 from transformers import BertTokenizer
 from huggingface_hub import hf_hub_download
 
-from .model import GPTConfig, GPT
-from .model_fine import FineGPT, FineGPTConfig
+from bark.model import GPTConfig, GPT
+from bark.model_fine import FineGPT, FineGPTConfig
 
 if (
-        torch.cuda.is_available() and
-        hasattr(torch.cuda, "amp") and
-        hasattr(torch.cuda.amp, "autocast") and
-        hasattr(torch.cuda, "is_bf16_supported") and
-        torch.cuda.is_bf16_supported()
+    torch.cuda.is_available() and
+    hasattr(torch.cuda, "amp") and
+    hasattr(torch.cuda.amp, "autocast") and
+    hasattr(torch.cuda, "is_bf16_supported") and
+    torch.cuda.is_bf16_supported()
 ):
     autocast = funcy.partial(torch.cuda.amp.autocast, dtype=torch.bfloat16)
 else:
@@ -30,12 +30,14 @@ else:
     def autocast():
         yield
 
+
 # hold models in global scope to lazy load
 global models
 models = {}
 
 global models_devices
 models_devices = {}
+
 
 CONTEXT_WINDOW_SIZE = 1024
 
@@ -48,6 +50,7 @@ N_FINE_CODEBOOKS = 8
 COARSE_RATE_HZ = 75
 
 SAMPLE_RATE = 24_000
+
 
 SUPPORTED_LANGS = [
     ("English", "en"),
@@ -71,9 +74,12 @@ for _, lang in SUPPORTED_LANGS:
         for n in range(10):
             ALLOWED_PROMPTS.add(f"{prefix}{lang}_speaker_{n}")
 
+
 logger = logging.getLogger(__name__)
 
-CUR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+CUR_PATH = "/usr/local/lib/python3.8/site-packages/bark"#os.path.dirname(os.path.abspath(__file__))
+
 
 default_cache_dir = os.path.join(os.path.expanduser("~"), ".cache")
 CACHE_DIR = os.path.join(os.getenv("XDG_CACHE_HOME", default_cache_dir), "suno", "bark_v0")
@@ -86,6 +92,7 @@ def _cast_bool_env_var(s):
 USE_SMALL_MODELS = _cast_bool_env_var(os.environ.get("SUNO_USE_SMALL_MODELS", "False"))
 GLOBAL_ENABLE_MPS = _cast_bool_env_var(os.environ.get("SUNO_ENABLE_MPS", "False"))
 OFFLOAD_CPU = _cast_bool_env_var(os.environ.get("SUNO_OFFLOAD_CPU", "False"))
+
 
 REMOTE_MODEL_PATHS = {
     "text_small": {
@@ -113,6 +120,7 @@ REMOTE_MODEL_PATHS = {
         "file_name": "fine_2.pt",
     },
 }
+
 
 if not hasattr(torch.nn.functional, 'scaled_dot_product_attention') and torch.cuda.is_available():
     logger.warning(
@@ -180,7 +188,7 @@ def clean_models(model_key=None):
     for k in model_keys:
         if k in models:
             del models[k]
-    _clear_cuda_cache()
+    #_clear_cuda_cache()
     gc.collect()
 
 
@@ -215,7 +223,7 @@ def _load_model(ckpt_path, device, use_small=False, model_type="text"):
     unwanted_prefix = "_orig_mod."
     for k, v in list(state_dict.items()):
         if k.startswith(unwanted_prefix):
-            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+            state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
     extra_keys = set(state_dict.keys()) - set(model.state_dict().keys())
     extra_keys = set([k for k in extra_keys if not k.endswith(".attn.bias")])
     missing_keys = set(model.state_dict().keys()) - set(state_dict.keys())
@@ -227,11 +235,11 @@ def _load_model(ckpt_path, device, use_small=False, model_type="text"):
     model.load_state_dict(state_dict, strict=False)
     n_params = model.get_num_params()
     val_loss = checkpoint["best_val_loss"].item()
-    logger.info(f"model loaded: {round(n_params / 1e6, 1)}M params, {round(val_loss, 3)} loss")
+    logger.info(f"model loaded: {round(n_params/1e6,1)}M params, {round(val_loss,3)} loss")
     model.eval()
     model.to(device)
     del checkpoint, state_dict
-    _clear_cuda_cache()
+    #_clear_cuda_cache()
     if model_type == "text":
         tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
         return {
@@ -246,7 +254,7 @@ def _load_codec_model(device):
     model.set_target_bandwidth(6.0)
     model.eval()
     model.to(device)
-    _clear_cuda_cache()
+    #_clear_cuda_cache()
     return model
 
 
@@ -293,18 +301,18 @@ def load_codec_model(use_gpu=True, force_reload=False):
 
 
 def preload_models(
-        text_use_gpu=True,
-        text_use_small=True,
-        coarse_use_gpu=True,
-        coarse_use_small=True,
-        fine_use_gpu=True,
-        fine_use_small=True,
-        codec_use_gpu=True,
-        force_reload=False,
+    text_use_gpu=True,
+    text_use_small=False,
+    coarse_use_gpu=True,
+    coarse_use_small=False,
+    fine_use_gpu=True,
+    fine_use_small=False,
+    codec_use_gpu=True,
+    force_reload=False,
 ):
     """Load all the necessary models for the pipeline."""
     if _grab_best_device() == "cpu" and (
-            text_use_gpu or coarse_use_gpu or fine_use_gpu or codec_use_gpu
+        text_use_gpu or coarse_use_gpu or fine_use_gpu or codec_use_gpu
     ):
         logger.warning("No GPU being used. Careful, inference might be very slow!")
     _ = load_model(
@@ -357,9 +365,9 @@ def _load_history_prompt(history_prompt_input):
             os.path.join(CUR_PATH, "assets", "prompts", f"{history_prompt_input}.npz")
         )
     elif isinstance(history_prompt_input, dict):
-        assert ("semantic_prompt" in history_prompt_input)
-        assert ("coarse_prompt" in history_prompt_input)
-        assert ("fine_prompt" in history_prompt_input)
+        assert("semantic_prompt" in history_prompt_input)
+        assert("coarse_prompt" in history_prompt_input)
+        assert("fine_prompt" in history_prompt_input)
         history_prompt = history_prompt_input
     else:
         raise ValueError("history prompt format unrecognized")
@@ -367,16 +375,16 @@ def _load_history_prompt(history_prompt_input):
 
 
 def generate_text_semantic(
-        text,
-        history_prompt=None,
-        temp=0.7,
-        top_k=None,
-        top_p=0.75,
-        silent=False,
-        min_eos_p=0.009,
-        max_gen_duration_s=None,
-        allow_early_stop=True,
-        use_kv_caching=False,
+    text,
+    history_prompt=None,
+    temp=0.7,
+    top_k=None,
+    top_p=None,
+    silent=False,
+    min_eos_p=0.2,
+    max_gen_duration_s=None,
+    allow_early_stop=True,
+    use_kv_caching=False,
 ):
     """Generate semantic tokens from text."""
     assert isinstance(text, str)
@@ -386,11 +394,11 @@ def generate_text_semantic(
         history_prompt = _load_history_prompt(history_prompt)
         semantic_history = history_prompt["semantic_prompt"]
         assert (
-                isinstance(semantic_history, np.ndarray)
-                and len(semantic_history.shape) == 1
-                and len(semantic_history) > 0
-                and semantic_history.min() >= 0
-                and semantic_history.max() <= SEMANTIC_VOCAB_SIZE - 1
+            isinstance(semantic_history, np.ndarray)
+            and len(semantic_history.shape) == 1
+            and len(semantic_history) > 0
+            and semantic_history.min() >= 0
+            and semantic_history.max() <= SEMANTIC_VOCAB_SIZE - 1
         )
     else:
         semantic_history = None
@@ -438,7 +446,7 @@ def generate_text_semantic(
         x = x.to(device)
         n_tot_steps = 768
         # custom tqdm updates since we don't know when eos will occur
-        pbar = tqdm.tqdm(disable=silent, total=100, desc="generate_text_semantic")
+        pbar = tqdm.tqdm(disable=silent, total=100)
         pbar_state = 0
         tot_generated_duration_s = 0
         kv_cache = None
@@ -480,8 +488,8 @@ def generate_text_semantic(
             probs = probs.to(inf_device)
             item_next = item_next.to(inf_device)
             if allow_early_stop and (
-                    item_next == SEMANTIC_VOCAB_SIZE
-                    or (min_eos_p is not None and probs[-1] >= min_eos_p)
+                item_next == SEMANTIC_VOCAB_SIZE
+                or (min_eos_p is not None and probs[-1] >= min_eos_p)
             ):
                 # eos found, so break
                 pbar.update(100 - pbar_state)
@@ -500,11 +508,11 @@ def generate_text_semantic(
                 pbar.update(req_pbar_state - pbar_state)
             pbar_state = req_pbar_state
         pbar.close()
-        out = x.detach().cpu().numpy().squeeze()[256 + 256 + 1:]
+        out = x.detach().cpu().numpy().squeeze()[256 + 256 + 1 :]
     if OFFLOAD_CPU:
         model.to("cpu")
     assert all(0 <= out) and all(out < SEMANTIC_VOCAB_SIZE)
-    _clear_cuda_cache()
+    #_clear_cuda_cache()
     return out
 
 
@@ -523,23 +531,23 @@ COARSE_INFER_TOKEN = 12_050
 
 
 def generate_coarse(
-        x_semantic,
-        history_prompt=None,
-        temp=0.7,
-        top_k=None,
-        top_p=None,
-        silent=False,
-        max_coarse_history=630,  # min 60 (faster), max 630 (more context)
-        sliding_window_len=60,
-        use_kv_caching=False,
+    x_semantic,
+    history_prompt=None,
+    temp=0.7,
+    top_k=None,
+    top_p=None,
+    silent=False,
+    max_coarse_history=630,  # min 60 (faster), max 630 (more context)
+    sliding_window_len=60,
+    use_kv_caching=False,
 ):
     """Generate coarse audio codes from semantic tokens."""
     assert (
-            isinstance(x_semantic, np.ndarray)
-            and len(x_semantic.shape) == 1
-            and len(x_semantic) > 0
-            and x_semantic.min() >= 0
-            and x_semantic.max() <= SEMANTIC_VOCAB_SIZE - 1
+        isinstance(x_semantic, np.ndarray)
+        and len(x_semantic.shape) == 1
+        and len(x_semantic) > 0
+        and x_semantic.min() >= 0
+        and x_semantic.max() <= SEMANTIC_VOCAB_SIZE - 1
     )
     assert 60 <= max_coarse_history <= 630
     assert max_coarse_history + sliding_window_len <= 1024 - 256
@@ -550,21 +558,21 @@ def generate_coarse(
         x_semantic_history = history_prompt["semantic_prompt"]
         x_coarse_history = history_prompt["coarse_prompt"]
         assert (
-                isinstance(x_semantic_history, np.ndarray)
-                and len(x_semantic_history.shape) == 1
-                and len(x_semantic_history) > 0
-                and x_semantic_history.min() >= 0
-                and x_semantic_history.max() <= SEMANTIC_VOCAB_SIZE - 1
-                and isinstance(x_coarse_history, np.ndarray)
-                and len(x_coarse_history.shape) == 2
-                and x_coarse_history.shape[0] == N_COARSE_CODEBOOKS
-                and x_coarse_history.shape[-1] >= 0
-                and x_coarse_history.min() >= 0
-                and x_coarse_history.max() <= CODEBOOK_SIZE - 1
-                and (
-                        round(x_coarse_history.shape[-1] / len(x_semantic_history), 1)
-                        == round(semantic_to_coarse_ratio / N_COARSE_CODEBOOKS, 1)
-                )
+            isinstance(x_semantic_history, np.ndarray)
+            and len(x_semantic_history.shape) == 1
+            and len(x_semantic_history) > 0
+            and x_semantic_history.min() >= 0
+            and x_semantic_history.max() <= SEMANTIC_VOCAB_SIZE - 1
+            and isinstance(x_coarse_history, np.ndarray)
+            and len(x_coarse_history.shape) == 2
+            and x_coarse_history.shape[0] == N_COARSE_CODEBOOKS
+            and x_coarse_history.shape[-1] >= 0
+            and x_coarse_history.min() >= 0
+            and x_coarse_history.max() <= CODEBOOK_SIZE - 1
+            and (
+                round(x_coarse_history.shape[-1] / len(x_semantic_history), 1)
+                == round(semantic_to_coarse_ratio / N_COARSE_CODEBOOKS, 1)
+            )
         )
         x_coarse_history = _flatten_codebooks(x_coarse_history) + SEMANTIC_VOCAB_SIZE
         # trim histories correctly
@@ -611,7 +619,7 @@ def generate_coarse(
         for _ in tqdm.tqdm(range(n_window_steps), total=n_window_steps, disable=silent):
             semantic_idx = base_semantic_idx + int(round(n_step / semantic_to_coarse_ratio))
             # pad from right side
-            x_in = x_semantic_in[:, np.max([0, semantic_idx - max_semantic_history]):]
+            x_in = x_semantic_in[:, np.max([0, semantic_idx - max_semantic_history]) :]
             x_in = x_in[:, :256]
             x_in = F.pad(
                 x_in,
@@ -639,10 +647,10 @@ def generate_coarse(
 
                 logits, kv_cache = model(x_input, use_cache=use_kv_caching, past_kv=kv_cache)
                 logit_start_idx = (
-                        SEMANTIC_VOCAB_SIZE + (1 - int(is_major_step)) * CODEBOOK_SIZE
+                    SEMANTIC_VOCAB_SIZE + (1 - int(is_major_step)) * CODEBOOK_SIZE
                 )
                 logit_end_idx = (
-                        SEMANTIC_VOCAB_SIZE + (2 - int(is_major_step)) * CODEBOOK_SIZE
+                    SEMANTIC_VOCAB_SIZE + (2 - int(is_major_step)) * CODEBOOK_SIZE
                 )
                 relevant_logits = logits[0, 0, logit_start_idx:logit_end_idx]
                 if top_p is not None:
@@ -674,47 +682,47 @@ def generate_coarse(
                 x_in = torch.cat((x_in, item_next[None]), dim=1)
                 del logits, relevant_logits, probs, item_next
                 n_step += 1
-            if len(x_coarse_in[0]) % N_COARSE_CODEBOOKS == 0:
-                print(x_coarse_in)
             del x_in
+            if n_window_steps == 100:
+                break
         del x_semantic_in
     if OFFLOAD_CPU:
         model.to("cpu")
-    gen_coarse_arr = x_coarse_in.detach().cpu().numpy().squeeze()[len(x_coarse_history):]
+    gen_coarse_arr = x_coarse_in.detach().cpu().numpy().squeeze()[len(x_coarse_history) :]
     del x_coarse_in
-    assert len(gen_coarse_arr) == n_steps
+    #assert len(gen_coarse_arr) == n_steps
     gen_coarse_audio_arr = gen_coarse_arr.reshape(-1, N_COARSE_CODEBOOKS).T - SEMANTIC_VOCAB_SIZE
     for n in range(1, N_COARSE_CODEBOOKS):
         gen_coarse_audio_arr[n, :] -= n * CODEBOOK_SIZE
-    _clear_cuda_cache()
+    #_clear_cuda_cache()
     return gen_coarse_audio_arr
 
 
 def generate_fine(
-        x_coarse_gen,
-        history_prompt=None,
-        temp=0.5,
-        silent=True,
+    x_coarse_gen,
+    history_prompt=None,
+    temp=0.5,
+    silent=True,
 ):
     """Generate full audio codes from coarse audio codes."""
     assert (
-            isinstance(x_coarse_gen, np.ndarray)
-            and len(x_coarse_gen.shape) == 2
-            and 1 <= x_coarse_gen.shape[0] <= N_FINE_CODEBOOKS - 1
-            and x_coarse_gen.shape[1] > 0
-            and x_coarse_gen.min() >= 0
-            and x_coarse_gen.max() <= CODEBOOK_SIZE - 1
+        isinstance(x_coarse_gen, np.ndarray)
+        and len(x_coarse_gen.shape) == 2
+        and 1 <= x_coarse_gen.shape[0] <= N_FINE_CODEBOOKS - 1
+        and x_coarse_gen.shape[1] > 0
+        and x_coarse_gen.min() >= 0
+        and x_coarse_gen.max() <= CODEBOOK_SIZE - 1
     )
     if history_prompt is not None:
         history_prompt = _load_history_prompt(history_prompt)
         x_fine_history = history_prompt["fine_prompt"]
         assert (
-                isinstance(x_fine_history, np.ndarray)
-                and len(x_fine_history.shape) == 2
-                and x_fine_history.shape[0] == N_FINE_CODEBOOKS
-                and x_fine_history.shape[1] >= 0
-                and x_fine_history.min() >= 0
-                and x_fine_history.max() <= CODEBOOK_SIZE - 1
+            isinstance(x_fine_history, np.ndarray)
+            and len(x_fine_history.shape) == 2
+            and x_fine_history.shape[0] == N_FINE_CODEBOOKS
+            and x_fine_history.shape[1] >= 0
+            and x_fine_history.min() >= 0
+            and x_fine_history.max() <= CODEBOOK_SIZE - 1
         )
     else:
         x_fine_history = None
@@ -733,7 +741,7 @@ def generate_fine(
         [
             x_coarse_gen,
             np.zeros((N_FINE_CODEBOOKS - n_coarse, x_coarse_gen.shape[1]))
-            + CODEBOOK_SIZE,  # padding
+            + CODEBOOK_SIZE  # padding
         ]
     ).astype(np.int32)
     # prepend history if available (max 512)
@@ -762,11 +770,11 @@ def generate_fine(
     n_loops = np.max([0, int(np.ceil((x_coarse_gen.shape[1] - (1024 - n_history)) / 512))]) + 1
     with _inference_mode():
         in_arr = torch.tensor(in_arr.T).to(device)
-        for n in tqdm.tqdm(range(n_loops), disable=silent, desc="generate_fine"):
+        for n in tqdm.tqdm(range(n_loops), disable=silent):
             start_idx = np.min([n * 512, in_arr.shape[0] - 1024])
             start_fill_idx = np.min([n_history + n * 512, in_arr.shape[0] - 512])
             rel_start_fill_idx = start_fill_idx - start_idx
-            in_buffer = in_arr[start_idx: start_idx + 1024, :][None]
+            in_buffer = in_arr[start_idx : start_idx + 1024, :][None]
             for nn in range(n_coarse, N_FINE_CODEBOOKS):
                 logits = model(nn, in_buffer)
                 if temp is None:
@@ -790,7 +798,7 @@ def generate_fine(
             # transfer over info into model_in and convert to numpy
             for nn in range(n_coarse, N_FINE_CODEBOOKS):
                 in_arr[
-                start_fill_idx: start_fill_idx + (1024 - rel_start_fill_idx), nn
+                    start_fill_idx : start_fill_idx + (1024 - rel_start_fill_idx), nn
                 ] = in_buffer[0, rel_start_fill_idx:, nn]
             del in_buffer
         gen_fine_arr = in_arr.detach().cpu().numpy().squeeze().T
@@ -800,8 +808,8 @@ def generate_fine(
     gen_fine_arr = gen_fine_arr[:, n_history:]
     if n_remove_from_end > 0:
         gen_fine_arr = gen_fine_arr[:, :-n_remove_from_end]
-    assert gen_fine_arr.shape[-1] == x_coarse_gen.shape[-1]
-    _clear_cuda_cache()
+    #assert gen_fine_arr.shape[-1] == x_coarse_gen.shape[-1]
+    #_clear_cuda_cache()
     return gen_fine_arr
 
 
@@ -826,9 +834,6 @@ def codec_decode(fine_tokens):
     if OFFLOAD_CPU:
         model.to("cpu")
     return audio_arr
-
-
-### Stream
 
 
 def generate_coarse_stream(
@@ -985,15 +990,15 @@ def generate_coarse_stream(
                 n_step += 1
             del x_in
             if len(x_coarse_in[0]) % N_COARSE_CODEBOOKS == 0:
-                print(x_coarse_in)
-                print("before:", x_coarse_in)
+                #print(x_coarse_in)
+                #print("before:", x_coarse_in)
                 x_coarse_in_copy = x_coarse_in.detach().clone()
                 gen_coarse_audio_arr = prepare_coarse_out(x_coarse_in_copy, x_coarse_history)
-                print("after:", x_coarse_in)
+                #print("after:", x_coarse_in)
                 yield gen_coarse_audio_arr
 
         del x_semantic_in
-    _clear_cuda_cache()
+    #_clear_cuda_cache()
 
 
 def prepare_coarse_out(x_coarse_in, x_coarse_history):
